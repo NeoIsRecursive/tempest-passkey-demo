7708436ff66349c09b4690588b4f67e91e2e6682
NeoIsRecursive <neo.leijondahl@gmail.com>
2025-10-09 12:41:14 +0200

    fix(ci): add default values to db config

--- app/databse.config.php
+++ app/databse.config.php
@@ -7,9 +7,9 @@ use Tempest\Database\Config\MysqlConfig;
 use function Tempest\env;
 
 return new MysqlConfig(
-    host: env('DB_HOST'),
-    port: env('DB_PORT'),
-    username: env('DB_USERNAME'),
-    password: env('DB_PASSWORD'),
-    database: env('DB_DATABASE'),
+    host: env('DB_HOST', 'localhost'),
+    port: env('DB_PORT', '3306'),
+    username: env('DB_USERNAME', 'dev'),
+    password: env('DB_PASSWORD', 'password'),
+    database: env('DB_DATABASE', 'dev'),
 );
