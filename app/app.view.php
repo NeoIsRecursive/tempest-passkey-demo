08e55932b118107660561a1f1a445afc4a84cd28
NeoIsRecursive <neo.leijondahl@gmail.com>
2025-10-09 11:30:43 +0200

    fix(ui): color scheme

--- app/app.view.php
+++ app/app.view.php
@@ -5,6 +5,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="color-scheme" content="dark light" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title inertia>Tempest</title>
     <x-vite-tags />
