514b7b086f6bc77a2293de1f98f9ec94db1c79cf
NeoIsRecursive <neo.leijondahl@gmail.com>
2026-09-25 14:54:50 +0200

    feat: change env var name to work with laravel cloud

--- app/database.config.php
+++ app/database.config.php
@@ -7,9 +7,9 @@ use Tempest\Database\Config\MysqlConfig;
 use function Tempest\env;
 
 return new MysqlConfig(
-    host: env('DATABASE_HOST', default: '127.0.0.1'),
-    port: env('DATABASE_PORT', default: '3306'),
-    username: env('DATABASE_USERNAME', default: ''),
-    password: env('DATABASE_PASSWORD', default: ''),
-    database: env('DATABASE_DATABASE', default: ''),
+    host: env('DB_HOST', default: '127.0.0.1'),
+    port: env('DB_PORT', default: '3306'),
+    username: env('DB_USERNAME', default: ''),
+    password: env('DB_PASSWORD', default: ''),
+    database: env('DB_DATABASE', default: ''),
 );
